f452ad314370ce2ff777e28ce7ed292efcf51a41
Akhand <akhandsingh0075@gmail.com>
2024-12-17 23:28:07 +0530

    Initial SetUp

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-500'>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
